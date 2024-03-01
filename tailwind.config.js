a09bf96522ff0ee276d67a28670121cd41f353e1
Kacper Grudziński <kgrvdzin@gmail.com>
2024-03-01 17:11:12 +0100

    Add semantic HTML & google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        kodeMono: ["Kode Mono", "monospace"]
+      }
+    },
   },
   plugins: [],
 }
