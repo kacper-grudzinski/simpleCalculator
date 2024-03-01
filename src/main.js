964411e67adc70379625d3d75f6285ddfb5efc74
Kacper Grudziński <kgrvdzin@gmail.com>
2024-03-01 23:44:09 +0100

    Add fix for calculations

--- src/main.js
+++ src/main.js
@@ -2,24 +2,44 @@ const $display = document.querySelector('input[type="text"]');
 const $buttons = document.querySelectorAll('input[type="button"]');
 
 $buttons.forEach(($button) => {
-    $button.addEventListener('click', () => {
-        const value = $button.value;
-    
-        if($display.value == '' && value == '0'){
-            $display.value = '';
-            return
-        }
+  $button.addEventListener("click", () => {
+    const value = $button.value;
+    let len = $display.value.length;
 
-        if(value === 'C') {
-            $display.value = '';
-            return;
-        }
+    if ($display.value == "" && value == "0") {
+      $display.value = "";
+      return;
+    }
 
-        if(value === '=') {
-            const express = $display.value.replace(/x/g, '*');
-            $display.value = eval(express);
-            return
-        }
-            $display.value += value;
-    })
-})
+    if (value === "C") {
+      $display.value = "";
+      return;
+    }
+
+    if (value === "=") {
+      const express = $display.value.replace(/x/g, "*");
+      $display.value = eval(express);
+      return;
+    }
+
+    if (len > 0) {
+      if (
+        ($display.value[len - 1] == "/" && value === "/") ||
+        ($display.value[len - 1] == "/" && value === "+") ||
+        ($display.value[len - 1] == "-" && value === "-") ||
+        ($display.value[len - 1] == "-" && value === "x") ||
+        ($display.value[len - 1] == "-" && value === "/") ||
+        ($display.value[len - 1] == "x" && value === "x") ||
+        ($display.value[len - 1] == "x" && value === "+") ||
+        ($display.value[len - 1] == "+" && value === "+") ||
+        ($display.value[len - 1] == "+" && value === "x") ||
+        ($display.value[len - 1] == "+" && value === "/")
+      ) {
+      } else {
+        $display.value += value;
+      }
+    } else if (len == 0) {
+      $display.value += value;
+    } 
+  });
+});
